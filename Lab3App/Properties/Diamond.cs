17b8d9f51e640db9b69495bd7ee9a240b1dd9fbf
mayagelbloom <145133295+mayagelbloom@users.noreply.github.com>
2024-02-02 17:32:14 -0500

    Add files via upload

---
+++ Lab3App/Properties/Diamond.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Lab3App
+{
+    internal class Diamond : Treasure
+    {
+        public Diamond(string description, int score) : base(score)
+        {
+            Description = description;
+        }
+        public void Display()
+        {
+            Console.WriteLine($"Diamond{Description} is displayed");
+
+        }
+    }
+}
---
+++ Lab3App/Properties/MagicWand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Lab3App
+{
+    internal class MagicWand : Tool
+    {
+        public override void DoAction()
+        {
+            Display();
+            Console.WriteLine("MagicWand is Used");
+        }
+    }
+}
